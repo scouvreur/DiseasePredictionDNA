2fc69d0b05a1f391be693ef7a2fa71d7785283c5
Stephane Couvreur <stephane.couvreur.sueron@gmail.com>
2018-01-22 18:08:05 +0000

    Model code started

---
+++ model.py
@@ -0,0 +1,18 @@
+# from sklearn import svm
+import pandas as pd
+import numpy as np
+# import tensorflow as tf
+import matplotlib.pyplot as plt
+import h5py
+from sklearn.metrics import roc_auc_score
+
+# def loadTrainingData():
+# 	'''
+# 	This array will be of 1D with length 784, the pixel intensity values
+# 	are integers from 0 to 255. We can therefore use unsigned 8-bit integers.
+# 	The array is reshaped to the image dimensions 28x28.
+# 	'''
+
+# loadTrainingData()
+
+train = np.zeros(())
