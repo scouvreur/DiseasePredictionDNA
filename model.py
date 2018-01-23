a0228fa086b8473086a64019d410d5e44bd2d0f0
Stephane Couvreur <stephane.couvreur.sueron@gmail.com>
2018-01-23 16:30:24 +0000

    Files before editing input data

--- model.py
+++ model.py
@@ -3,7 +3,7 @@
 import numpy as np
 # import tensorflow as tf
 # import matplotlib.pyplot as plt
-# import h5py
+import h5py
 # from sklearn.metrics import roc_auc_score
 
 # def loadTrainingData():
@@ -15,7 +15,25 @@ import numpy as np
 
 # loadTrainingData()
 
-train = np.zeros((10,5,2))
+train = np.zeros((26499+1,2*(18123+1))) # initialize an empty 2D array
+print('start processing traindata')
+with open('train.csv') as trainfile:
+    for index,line in enumerate(trainfile):
+	    # traindata format
+	    # Ids,SNP_0_a,SNP_0_b,SNP_0_a,SNP_0_b, [...] ,SNP_18123_a,SNP_18123_b
+	    # ID0,1,0,0,0, [...] ,1,0
+
+	    traindata = line.split(',')
+	    try:
+	    	ID = str(traindata[0])
+	    	SNP = traindata[1:]
+	    	train[ID-1] = SNP # write values into 2D array
+
+	    	if index % 1000 == 0:
+	            print(index,"lines have been processed")
+	    except ValueError:
+	        print("found line with datatype error! skip this line")
+	        continue
 
 # As the SNP data is binary, can be stored
 # efficiently in 8-bit integer format
