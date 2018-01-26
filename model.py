b02fa0628d4f58d41712714f0f1753be7c328fa7
Stephane Couvreur <stephane.couvreur.sueron@gmail.com>
2018-01-26 14:52:30 +0000

    Model updated

--- model.py
+++ model.py
@@ -6,35 +6,29 @@ import numpy as np
 import h5py
 # from sklearn.metrics import roc_auc_score
 
-# def loadTrainingData():
+# def loadData():
 # 	'''
 # 	This array will be of 1D with length 784, the pixel intensity values
 # 	are integers from 0 to 255. We can therefore use unsigned 8-bit integers.
 # 	The array is reshaped to the image dimensions 28x28.
 # 	'''
-train = np.zeros((26499+1,2*(18123+1))) # initialize an empty 2D array
-train = np.zeros((26499+1)) # initialize an empty 2D array
-# train = []
+# train = np.zeros((26499+1,2*(18123+1))) # initialize an empty 2D array
+train = np.array([])
 print('start processing traindata')
-with open('trainTEST.csv') as trainfile:
-    for index,line in enumerate(trainfile):
-	    # traindata format
-	    # Ids,SNP_0_a,SNP_0_b,SNP_0_a,SNP_0_b, [...] ,SNP_18123_a,SNP_18123_b
-	    # ID0,1,0,0,0, [...] ,1,0
+with open('train.csv') as trainfile:
+	lines = trainfile.readlines()
+	for line in lines:
+		# traindata format
+		# Ids,SNP_0_a,SNP_0_b,SNP_0_a,SNP_0_b, [...] ,SNP_18123_a,SNP_18123_b
+		# ID0,1,0,0,0, [...] ,1,0
 
-	    traindata = line.split(',')
-	    try:
-	    	ID = int(traindata[0])
-	    	SNP = np.array(traindata[1:-1], dtype='uint8')
-	    	train[ID] = SNP # write values into 2D array
+		line = np.fromstring(line, dtype='int32', sep=',')
+		print(line)
+		train = np.append(train, line)
 
-	    	if index % 1000 == 0:
-	            print(index,"lines have been processed")
-	    except ValueError:
-	        print("found line with datatype error! skip this line")
-	        continue
+train = train.reshape(4, 36249)
 
-# loadTrainingData()
+# loadData()
 
 # As the SNP data is binary, can be stored
 # efficiently in 8-bit integer format
